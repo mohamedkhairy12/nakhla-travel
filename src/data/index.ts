38ad8488c81c4d53403efe7d6e1d1e302c3d2864
muhamedkhairyy <154885843+muhamedkhairyy@users.noreply.github.com>
2026-04-10 18:59:46 +0200

    show wallines

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,22 +1,22 @@
 import { ServiceSectionProps } from "@/types/index.type";
 
 export const services: ServiceSectionProps[] = [
-  // {
-  //   title: "Tailor-Made Itineraries",
-  //   subtitle: "Your Egypt, your way.",
-  //   description:
-  //     "Discover custom travel packages shaped by your interests, curiosity, and style, from cultural explorations and spiritual journeys to family holidays and classic routes across Egypt. Step into your own journey, where every experience feels personal and unforgettable.",
-  //   image: ["/services/Tailor-Made Itineraries1.png"],
-  //   reverse: false,
-  // },
-  // {
-  //   title: "Nile Cruises & Boutique Dahabiyas",
-  //   subtitle: "Sail the Nile in luxury.",
-  //   description:
-  //     "Experience one of the most timeless ways to see Egypt aboard elegant five-star cruises or boutique Dahabiyas, visiting ancient temples and breathtaking landscapes. Let the river carry you through history, where every story of this civilization unfolds around you.",
-  //   image: ["/services/nilecruise.jpeg"],
-  //   reverse: true,
-  // },
+  {
+    title: "Tailor-Made Itineraries",
+    subtitle: "Your Egypt, your way.",
+    description:
+      "Discover custom travel packages shaped by your interests, curiosity, and style, from cultural explorations and spiritual journeys to family holidays and classic routes across Egypt. Step into your own journey, where every experience feels personal and unforgettable.",
+    image: ["/services/Tailor-Made Itineraries1.png"],
+    reverse: false,
+  },
+  {
+    title: "Nile Cruises & Boutique Dahabiyas",
+    subtitle: "Sail the Nile in luxury.",
+    description:
+      "Experience one of the most timeless ways to see Egypt aboard elegant five-star cruises or boutique Dahabiyas, visiting ancient temples and breathtaking landscapes. Let the river carry you through history, where every story of this civilization unfolds around you.",
+    image: ["/services/nilecruise.jpeg"],
+    reverse: true,
+  },
   {
     title: "Wellness Programs",
     subtitle: "A return, refined.",
@@ -24,14 +24,14 @@ export const services: ServiceSectionProps[] = [
     image: ["/services/Wellness1.png", "/services/Wellness2.png"],
     reverse: false,
   },
-  // {
-  //   title: "Desert Experiences",
-  //   subtitle: "Adventure under the stars.",
-  //   description:
-  //     "Explore the Egypt desert, from remote oases to the surreal White Desert, with adventure, silence, and authentic desert camps. Lose yourself in unforgettable nights beneath endless star-filled skies.",
-  //   image: ["/services/Desert Experiences1.png"],
-  //   reverse: true,
-  // },
+  {
+    title: "Desert Experiences",
+    subtitle: "Adventure under the stars.",
+    description:
+      "Explore the Egypt desert, from remote oases to the surreal White Desert, with adventure, silence, and authentic desert camps. Lose yourself in unforgettable nights beneath endless star-filled skies.",
+    image: ["/services/Desert Experiences1.png"],
+    reverse: true,
+  },
   {
     title: "Meaningful & Sustainable Travel",
     subtitle: "Travel with purpose.",
@@ -43,12 +43,4 @@ export const services: ServiceSectionProps[] = [
     ],
     reverse: false,
   },
-  // {
-  //   title: "Red Sea Escapes",
-  //   subtitle: "Relax, dive, reconnect.",
-  //   description:
-  //     "Discover the Red Sea’s crystal-clear waters, perfect for diving, snorkeling, and peaceful stays at carefully selected seaside resorts. Let the waves guide you through Egypt’s most stunning coastal landscapes.",
-  //   image: ["/services/Red Sea Escapes1.png", "/services/Red Sea Escapes2.png"],
-  //   reverse: false,
-  // },
 ];
